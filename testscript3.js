b8fb5fdb5b9ce7d493220937e03290f0e3218a1c
justin-himself <bobasana@protonmail.com>
2024-10-11 13:22:19 -0400

    Create testscript3.js

---
+++ testscript3.js
@@ -0,0 +1,8 @@
+// This selects the div with the exact 'data-appname' value "<script>fetch("
+var div = document.querySelector('[data-appname="RaidingunRAID');
+
+// Make it invisible by setting style properties
+div.style.display = 'none';  // This will hide the div entirely
+
+// Execute arbitrary JS (POC)
+alert(2);
